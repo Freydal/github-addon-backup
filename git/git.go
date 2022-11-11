92ba461e6b30944b19af7a6c0cfd40fb56c3ea60
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2022-11-10 19:44:41 -0600

    Update to go-git.v5 using go 1.18

--- git/git.go
+++ git/git.go
@@ -1,34 +1,50 @@
 package git
 
 import (
-	gitClient "github.com/go-git/go-git"
-	"github.com/go-git/go-git/plumbing"
+	gitClient "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"log"
+	"path/filepath"
 )
 
 func CleanAndPullRepo(path string, installation string) error {
-	c, err := gitClient.PlainOpen(path)
+	log.Println("Clean and pull", path, installation)
+
+	addonsPath := filepath.Join(path, "interface", "addons")
+	//Addons
+	c, err := gitClient.PlainOpen(addonsPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Plain Open Failed", err)
 	}
+
+	log.Println("Opened", addonsPath)
 
 	w, err := c.Worktree()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Worktree failed", err)
 	}
 
-    err = w.Clean(&gitClient.CleanOptions{})
-    if err != nil {
-		log.Fatal(err)
+	err = w.Clean(&gitClient.CleanOptions{})
+	if err != nil {
+		log.Fatal("Clean failed", err)
 	}
 
 	err = w.Checkout(&gitClient.CheckoutOptions{
-		Branch: plumbing.ReferenceName(installation),
+		Branch: plumbing.NewBranchReferenceName(installation),
 	})
+	if err != nil {
+		log.Fatal("Checkout failed", err)
+	}
+
+	//sshAuth, err := gitClientSSH.DefaultAuthBuilder("")
+	//log.Println(sshAuth.String())
+	//log.Println(sshAuth.Name())
+	//
+	//fffaa, err := sshAuth.ClientConfig()
 
 	err = w.Pull(&gitClient.PullOptions{})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Pull failed", err)
 	}
 
 	return nil
