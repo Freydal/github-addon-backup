f15e523d74367a73f129d8b59faa6e9dc202cd0c
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2022-11-10 20:34:35 -0600

    Got a rough impl of the push

--- git/git.go
+++ git/git.go
@@ -5,12 +5,13 @@ import (
 	gitClient "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"log"
+	"time"
 )
 
 func CleanAndPullRepo(path string, branch string) error {
 	c, err := gitClient.PlainOpen(path)
 	if err != nil {
-		if err.Error() == "repository does not exist" {
+		if err == gitClient.ErrRepositoryNotExists {
 			return nil
 		}
 		return err
@@ -37,12 +38,56 @@ func CleanAndPullRepo(path string, branch string) error {
 
 	err = w.Pull(&gitClient.PullOptions{})
 	if err != nil {
-		if err.Error() == "already up-to-date" {
+		if err == gitClient.NoErrAlreadyUpToDate {
 			log.Println("Up to date!")
 		} else {
 			return err
 		}
 	}
+
+	return nil
+}
+
+func DailyPush(path string, branch string) error {
+	c, err := gitClient.PlainOpen(path)
+	if err != nil {
+		if err == gitClient.ErrRepositoryNotExists {
+			return nil
+		}
+		return err
+	}
+	log.Println(fmt.Sprintf("Pushing %s (%s)", path, branch))
+
+	w, err := c.Worktree()
+	if err != nil {
+		return err
+	}
+
+	err = w.Checkout(&gitClient.CheckoutOptions{
+		Branch: plumbing.NewBranchReferenceName(branch),
+		Create: true,
+		Keep:   true,
+	})
+	if err != nil {
+		return err
+	}
+
+	a, err := w.Status()
+	if a.IsClean() {
+		return nil
+	}
+
+	err = w.AddWithOptions(&gitClient.AddOptions{All: true})
+	if err != nil {
+		return err
+	}
+
+	hash, err := w.Commit(time.Now().Format("_1-_2-06"), &gitClient.CommitOptions{})
+	if err != nil {
+		return err
+	}
+
+	log.Println(hash)
 
 	return nil
 }
