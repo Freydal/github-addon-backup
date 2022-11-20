5de95708c5b78deb1bd7bcce3a8a89a82fa336f4
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2022-11-19 20:40:23 -0600

    Using new shared looping function

--- installations/installations.go
+++ installations/installations.go
@@ -9,79 +9,16 @@ import (
 )
 
 func KeepUpToDate(rootPath string) {
-	rootFile, err := os.Open(rootPath)
+	err := wowDirectoryWalk(rootPath, git.CleanAndPullRepo)
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	dirs, err := rootFile.Readdirnames(0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, dir := range dirs {
-		if strings.HasPrefix(dir, "_") {
-			//Addons
-			addonsPath := filepath.Join(rootPath, dir, "interface", "addons")
-			if err := git.CleanAndPullRepo(addonsPath, dir); err != nil {
-				log.Println(err)
-			}
-
-			wtfAccountPath := filepath.Join(rootPath, dir, "WTF", "Account")
-			wtfAccount, err := os.Open(wtfAccountPath)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			accounts, err := wtfAccount.Readdirnames(0)
-			if err != nil {
-				log.Fatal(err)
-			}
-			for _, account := range accounts {
-				accountPath := filepath.Join(wtfAccountPath, account)
-
-				if err := git.CleanAndPullRepo(accountPath, dir); err != nil {
-					log.Println(err)
-				}
-			}
-		}
 	}
 }
 
 func UpdateDaily(rootWowDirPath string) {
-	rootWowDirFile, err := os.Open(rootWowDirPath)
+	err := wowDirectoryWalk(rootWowDirPath, git.DailyPush)
 	if err != nil {
 		log.Fatal(err)
-	}
-
-	dirs, err := rootWowDirFile.Readdirnames(0)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, dir := range dirs {
-		if strings.HasPrefix(dir, "_") {
-			//Addons
-			addonsPath := filepath.Join(rootWowDirPath, dir, "interface", "addons")
-			if err := git.DailyPush(addonsPath, dir); err != nil {
-				log.Println(err)
-			}
-
-			wtfAccountPath := filepath.Join(rootWowDirPath, dir, "WTF", "Account")
-			wtfAccount, err := os.Open(wtfAccountPath)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			accounts, err := wtfAccount.Readdirnames(0)
-			if err != nil {
-				log.Fatal(err)
-			}
-			for _, account := range accounts {
-				accountPath := filepath.Join(wtfAccountPath, account)
-				if err := git.DailyPush(accountPath, dir); err != nil {
-					log.Println(err)
-				}
-			}
-		}
 	}
 }
 
