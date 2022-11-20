95d152f226910fccd2fae8de4f7147ea68b10b6c
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2022-11-19 20:39:27 -0600

    Adding general function for actions to be run across relevant folders in repo

--- installations/installations.go
+++ installations/installations.go
@@ -84,3 +84,47 @@ func UpdateDaily(rootWowDirPath string) {
 		}
 	}
 }
+
+func wowDirectoryWalk(rootPath string, action git.Action) error {
+	rootFile, err := os.Open(rootPath)
+	if err != nil {
+		return err
+	}
+
+	dirs, err := rootFile.Readdirnames(0)
+	if err != nil {
+		return err
+	}
+	for _, dir := range dirs {
+		if strings.HasPrefix(dir, "_") {
+			//Addons
+			addonsPath := filepath.Join(rootPath, dir, "interface", "addons")
+			if err := action(addonsPath, dir); err != nil {
+				log.Println(err)
+			}
+
+			wtfAccountPath := filepath.Join(rootPath, dir, "WTF", "Account")
+			wtfAccount, err := os.Open(wtfAccountPath)
+			if err != nil {
+				return err
+			}
+
+			accounts, err := wtfAccount.Readdirnames(0)
+			if err != nil {
+				return err
+			}
+			for _, account := range accounts {
+				if account == "SavedVariables" {
+					continue
+				}
+
+				accountPath := filepath.Join(wtfAccountPath, account)
+
+				if err := action(accountPath, dir); err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}
+	return nil
+}
