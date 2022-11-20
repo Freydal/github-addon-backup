3ca4e1412126e57e0725db91bb7f522c3045cb37
Nick Freyaldenhoven <freyaldenhovennc@gmail.com>
2022-11-19 20:05:20 -0600

    Finishing the pull working method.

--- git/git.go
+++ git/git.go
@@ -3,6 +3,7 @@ package git
 import (
 	"fmt"
 	gitClient "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
 	"log"
 	"time"
@@ -17,33 +18,123 @@ func CleanAndPullRepo(path string, branch string) error {
 		return err
 	}
 
-	log.Println(fmt.Sprintf("Clean and pull %s (%s)", path, branch))
-
 	w, err := c.Worktree()
 	if err != nil {
 		return err
 	}
 
-	err = w.Clean(&gitClient.CleanOptions{})
+	head, err := c.Head()
 	if err != nil {
 		return err
 	}
 
-	err = w.Checkout(&gitClient.CheckoutOptions{
-		Branch: plumbing.NewBranchReferenceName(branch),
-	})
+	status, err := w.Status()
 	if err != nil {
 		return err
+	}
+
+	if !status.IsClean() {
+		log.Println(fmt.Sprintf("Cleaning %s(%s)", path, head.Name()))
+		err = w.Clean(&gitClient.CleanOptions{
+			Dir: true,
+		})
+		if err != nil {
+			log.Println("Failed to clean")
+			return err
+		}
+
+		err = w.Checkout(&gitClient.CheckoutOptions{
+			Branch: head.Name(),
+			Force:  true,
+		})
+		if err != nil {
+			log.Println("Failed to checkout to reset status")
+			return err
+		}
+	}
+
+	branchRefName := plumbing.NewBranchReferenceName(branch)
+
+	if head.Name() != branchRefName {
+		log.Println(fmt.Sprintf("Checking out branch %s(%s -> %s)", path, head.Name(), branchRefName))
+		err = w.Checkout(&gitClient.CheckoutOptions{
+			Branch: branchRefName,
+			Force:  true,
+		})
+
+		if err != nil {
+			if err == plumbing.ErrReferenceNotFound {
+				err = c.Fetch(&gitClient.FetchOptions{})
+				if err != nil && err != gitClient.NoErrAlreadyUpToDate {
+					return err
+				}
+
+				cfg, err := c.Config()
+				if err != nil {
+					return err
+				}
+
+				configBranch, ok := cfg.Branches[branch]
+
+				if !ok {
+					configBranch = &config.Branch{
+						Name:   branch,
+						Remote: "origin",
+						Merge:  branchRefName,
+					}
+					err = c.CreateBranch(configBranch)
+
+					if err != nil {
+						return err
+					}
+				}
+
+				err = w.Checkout(&gitClient.CheckoutOptions{
+					Branch: branchRefName,
+					Create: true,
+					Force:  true,
+				})
+				if err != nil {
+					return err
+				}
+			} else {
+				return err
+			}
+		}
 	}
 
 	err = w.Pull(&gitClient.PullOptions{})
 	if err != nil {
 		if err == gitClient.NoErrAlreadyUpToDate {
-			log.Println("Up to date!")
+			log.Println(fmt.Sprintf("Up to date! %s(%s)", path, head.Name()))
+		} else if err == gitClient.ErrNonFastForwardUpdate {
+
+			reference, err := c.Storer.Reference(plumbing.NewRemoteReferenceName("origin", branch))
+			if err != nil {
+				return err
+			}
+
+			err = w.Reset(&gitClient.ResetOptions{
+				Mode:   gitClient.HardReset,
+				Commit: reference.Hash(),
+			})
+			if err != nil {
+				return err
+			}
+			log.Println(fmt.Sprintf("Hard Reset to origin! %s(%s)", path, head.Name()))
+			log.Println(fmt.Sprintf("Cleaning after reset %s(%s)", path, head.Name()))
+			err = w.Clean(&gitClient.CleanOptions{
+				Dir: true,
+			})
+			if err != nil {
+				log.Println("Failed to clean")
+				return err
+			}
 		} else {
 			return err
 		}
 	}
+	log.Println(fmt.Sprintf("Pulled %s(%s)", path, head.Name()))
 
 	return nil
 }
